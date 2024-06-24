afa17979ff3091603437095f1718222cab0c1ab4
choibohyun <320fleur@gmail.com>
2024-06-24 19:44:19 +0900

    first commit

---
+++ src/main/java/com/bh/hhplus_week_02/HhplusWeek02Application.java
@@ -0,0 +1,13 @@
+package com.bh.hhplus_week_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusWeek02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusWeek02Application.class, args);
+    }
+
+}
