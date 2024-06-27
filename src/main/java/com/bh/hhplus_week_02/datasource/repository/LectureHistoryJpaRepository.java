6a1d3b964301c8cb24facf4ee8415223539907a3
choibohyun <320fleur@gmail.com>
2024-06-28 06:58:15 +0900

    STEP04 특강 신청시, 동시성 처리 비즈니스 로직 추가

--- src/main/java/com/bh/hhplus_week_02/datasource/repository/LectureHistoryJpaRepository.java
+++ src/main/java/com/bh/hhplus_week_02/datasource/repository/LectureHistoryJpaRepository.java
@@ -2,10 +2,15 @@ package com.bh.hhplus_week_02.datasource.repository;
 
 import com.bh.hhplus_week_02.business.dto.LectureHistoryDto;
 import com.bh.hhplus_week_02.datasource.entity.LectureHistory;
+import jakarta.persistence.LockModeType;
+import org.hibernate.LockMode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface LectureHistoryJpaRepository extends JpaRepository<LectureHistory, Long> {
+
     void save(LectureHistoryDto lectureHistoryDto);
     boolean existsByUserIdAndLectureId(Long userId, Long lectureId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     int countLectureHistoryByLectureId(Long lectureId);
 }
